07eec049d71ff9c8da91401dae18be333e0a6239
Scott Hale <scott.hale@wipro.com>
2021-01-07 23:44:40 -0500

    initial commit

---
+++ cart.js
@@ -0,0 +1,177 @@
+class Cart{
+
+  constructor(){
+    this.cart = {}
+    this.currency_format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+    this.cartTotal;
+  }
+
+  removeItem = (item_key) => {
+    delete this.cart[item_key];
+  }
+
+  addItem = (item_id, item_size, item_qty, item_cost) => {
+    if(this.cart[item_id+"_"+item_size]){
+      this.updateItem(item_id+"_"+item_size, item_qty);
+    }
+    else{
+      this.cart[item_id+"_"+item_size] = {id:item_id, size:item_size, qty: Number(item_qty), cost: Number(item_cost)};
+    }
+  }
+
+  updateItem = (item_key, item_qty) => {
+    this.cart[item_key].qty = Number(this.cart[item_id].qty) + Number(item_qty);
+  }
+
+  _removeChildNodes = (container) =>{
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+  }
+
+  getCartContents = (container) =>{
+    this.cartTotal = 0;
+    if(container.hasChildNodes()){
+      this._removeChildNodes(container);
+    }
+
+    let header_row = document.createElement('div');
+    header_row.classList.add("cart-header-row");
+
+    let header_item_name = document.createElement('div');
+    header_item_name.classList.add("cart-header-name");
+    header_item_name.innerHTML = "Item Name";
+    header_row.append(header_item_name);
+
+    let header_item_size = document.createElement('div');
+    header_item_size.classList.add("cart-header-size");
+    header_item_size.innerHTML = "Item Size"
+    header_row.append(header_item_size);
+
+    let header_item_qty = document.createElement('div');
+    header_item_qty.classList.add("cart-header-qty");
+    header_item_qty.innerHTML = "Quantity"
+    header_row.append(header_item_qty);
+
+    let header_item_cost = document.createElement('div');
+    header_item_cost.classList.add("cart-header-cost");
+    header_item_cost.innerHTML = "Cost"
+    header_row.append(header_item_cost);
+
+    let header_item_remove = document.createElement('div');
+    header_item_remove.classList.add("cart-header-remove");
+    header_row.append(header_item_remove);
+
+    container.append(header_row);
+    container.append(document.createElement('hr'));
+
+    let keys = Object.keys(this.cart);
+    
+    keys.map((key)=>{
+      let item_row = document.createElement('div');
+      item_row.classList.add("cart-item-row");
+
+      let item_name = document.createElement('div');
+      item_name.classList.add("cart-item-name");
+      item_name.innerHTML = this.cart[key].id;
+      item_row.append(item_name);
+
+      let item_size = document.createElement('div');
+      item_size.classList.add("cart-item-size");
+      item_size.innerHTML = this.cart[key].size;
+      item_row.append(item_size);
+
+      let item_qty = document.createElement('div');
+      item_qty.classList.add("cart-item-qty");
+      item_qty.innerHTML = this.cart[key].qty;
+      item_row.append(item_qty);
+
+      let item_cost = document.createElement('div');
+      item_cost.classList.add("cart-item-cost");
+      item_cost.innerHTML = this._getTotalItemCost(this.cart[key]);
+      item_row.append(item_cost);
+
+      let item_remove = document.createElement('div');
+      item_remove.classList.add("cart-item-remove");
+
+      let item_remove_link = document.createElement('a');
+      item_remove_link.href = "javascript:window.removeFromCart('" + this.cart[key].id +"_"+this.cart[key].size +"')";
+      item_remove_link.innerHTML = "x";
+      item_remove.append(item_remove_link);
+
+      item_row.append(item_remove);
+
+      container.append(item_row);
+    });
+
+    if(keys.length==0){
+      let no_item_row = document.createElement('div');
+      no_item_row.classList.add("no-items-row");
+      no_item_row.innerHTML = "No items in cart."
+      container.append(no_item_row);
+    }
+
+    container.append(document.createElement('hr'));
+
+    let total_row = document.createElement('div');
+    total_row.classList.add("cart-total-row");
+
+    let total_remove = document.createElement('div');
+    total_remove.classList.add("cart-item-remove");
+    total_row.append(total_remove);
+
+    let total_cost = document.createElement('div');
+    total_cost.classList.add("cart-item-cost");
+    total_cost.innerHTML = this.currency_format.format(this.cartTotal);
+    total_row.append(total_cost);
+
+
+    
+    container.append(total_row);
+  }
+
+  _getTotalItemCost = (item) => {
+    let cost = Number(item.qty) * Number(item.cost);
+    this.cartTotal += cost;
+    return this.currency_format.format(cost);
+  }
+
+  getCartItemsCount = () =>{
+    let item_count = 0;
+    let keys = Object.keys(this.cart);
+    keys.map((key)=>{
+      item_count = item_count + Number(this.cart[key].qty);
+    });
+    return item_count;
+  }
+
+}
+
+if(!window.cart){
+  window.cart = new Cart();
+
+  window.addToCart = (form) => {
+    // need to transform from form to js here
+    window.cart.addItem(item_id, item_qty, item_size, item_cost);
+  }
+
+  window.removeFromCart = (item_id) =>{
+    window.cart.removeItem(item_id);
+    window.showCart();
+  }
+
+  window.showCart = () =>{
+     let container = document.getElementById("cart-container");
+     window.cart.getCartContents(container);
+     container.style.display = "block";
+  }
+
+  window.hideCart = () =>{
+    let container = document.getElementById("cart-container");
+    container.style.display = "none";
+  }
+
+  window.getItemsCount = () =>{
+    window.cart.getCartItemsCount();
+  }  
+}
